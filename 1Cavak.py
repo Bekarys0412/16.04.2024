1b51c2c93dd3b4eccbb75bca4fc9ccf77dcbfb5d
Admin <Admin@DESKTOP-4FJ5IJL>
2024-04-22 16:45:39 +0600

    first commit

---
+++ 1Cavak.py
@@ -0,0 +1,4 @@
+def add(x,y):
+    return x+y
+a=add(4,5)
+print(a)
---
+++ 3Cavak.py
@@ -0,0 +1,9 @@
+def add_to_list (x):
+    list_1=[{"name":"Kanat","last_name":"Erbol","Age":20 }, 
+   {"name":"Askar","last_name":"Erzhanov","Age":15},
+   {"name":"Kairat","last_name":"Zhandosov","Age":45}
+   ]
+    list_1.append(x)
+    return list_1
+print(add_to_list({"name":"jonbek","last_name":"Erbolat","Age":17}))
+  
